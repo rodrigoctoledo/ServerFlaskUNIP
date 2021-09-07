ca3cc74f931591c2f47cc8e5888c12d692f15dd7
rodrigoctoledo <SEU rodrigo_toledo@Outlook.com>
2021-09-07 20:55:33 -0300

    Alteração do site2

---
+++ djangofundamentos/tw_clientes/clientes/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Cliente
+from .forms import ClienteForm
+
+# Create your views here.
+
+def listar_clientes(request):
+    clientes = Cliente.objects.all()
+    return render(request,'clientes/lista_clientes.html',{'clientes':clientes})
+
+def inserir_cliente(request):
+
+    if request.method == "POST":
+        form = ClienteForm(request.POST)
+        if form.is_valid():
+            form.save()
+    form = ClienteForm()
+    return render(request,'clientes/form_cliente.html',{'form':form})
