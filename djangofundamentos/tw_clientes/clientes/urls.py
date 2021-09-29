469d472c364a362bcb00c03560e52b0512296247
rodrigoctoledo <rodrigo_toledo@outlook.com>
2021-09-28 21:59:30 -0300

    Delete djangofundamentos directory

--- djangofundamentos/tw_clientes/clientes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns =[
-
-    path('listar', listar_clientes, name='listar_clientes'),
-    path('cadastrar',inserir_cliente,name='cadastrar_cliente')
-]
